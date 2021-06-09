1eb95c6245a59c8324a3c781994226b64e30dc2a
Mike Keen <mwk@mikekeen.com>
2021-06-08 20:22:01 -0400

    lets do it shittily

--- wsgi.py
+++ wsgi.py
@@ -4,13 +4,21 @@ import cherrypy
 
 import saltbucket
 
+logger = logging.getLogger(__name__)
+
+class app(object):
+  @cherrypy.expose
+  def index(self):
+      return "Hello World!"
+
+
 def get_application(*args):
   opts_tuple = args
 
   def wsgi_app(environ, start_response):
-      cherrypy.config.update({"environment": "embedded"})
-      cherrypy.tree.mount(saltbucket, "/", {})
-      return cherrypy.tree(environ, start_response)
+    cherrypy.config.update({"environment": "embedded"})
+    cherrypy.tree.mount(app, "/", {})
+    return cherrypy.tree(environ, start_response)
 
   return wsgi_app
 
